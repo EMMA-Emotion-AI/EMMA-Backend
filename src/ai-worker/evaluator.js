7d9642d662025a506799955f38b6d63b1742bf53
NullDev (Chris) <shadow@nulldev.org>
2019-12-19 12:00:12 +0100

    Added stats route

--- src/ai-worker/evaluator.js
+++ src/ai-worker/evaluator.js
@@ -4,11 +4,22 @@
 // = Copyright (c) EMMA = //
 // ====================== //
 
-let prettify = (arr) => JSON.stringify(JSON.parse(JSON.stringify(arr)), null, 4);
+// Utils
+let isset = (obj) => !!(obj && obj !== null && (typeof obj === "string" || typeof obj === "number") || obj === 0);
 
+// Entry point
 let evaluate = function(req, res, app){
     let text = req.query.text;
 
+    if (!isset(text)){
+        let respone = {
+            "error": "no text provided"
+        };
+
+        return res.set({
+            "Content-Type": "application/json; charset=utf-8"
+        }).status(400).send(respone);
+    }
 };
 
 module.exports = {
--- src/routes/route/stats.js
+++ src/routes/route/stats.js
@@ -4,8 +4,35 @@
 // = Copyright (c) EMMA = //
 // ====================== //
 
-let stats = function(){
+// Utils
+let meta = require("../../utils/meta");
 
+/**
+ * Sends status informations and meta data
+ *
+ * @param {*} req
+ * @param {*} res
+ * @returns
+ */
+let stats = function(req, res){
+    meta((data) => {
+        let respone = {
+            "status": "ok",
+            "nodejs_vserion": data.nodeversion,
+            "current_pid": process.pid,
+            "uptime_seconds": Math.floor(process.uptime()),
+            "operating_system": data.os,
+            "system_cpu_usage_time": process.cpuUsage().system,
+            "user_cpu_usage_time": process.cpuUsage().user,
+            "arch": process.arch,
+            "server_ip": data.ip,
+            "client_ip": (req.headers["x-forwarded-for"] || req.connection.remoteAddress)
+        };
+
+        return res.set({
+            "Content-Type": "application/json; charset=utf-8"
+        }).status(200).send(respone);
+    });
 };
 
 module.exports = stats;
