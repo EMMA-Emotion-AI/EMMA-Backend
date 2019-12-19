b186d657a57335b59bbf1733597c1496261dff2b
NullDev (Chris) <shadow@nulldev.org>
2019-12-19 12:29:20 +0100

    added base logic

--- src/ai-worker/classifier.js
+++ src/ai-worker/classifier.js
@@ -14,6 +14,38 @@ class Classifier {
 
     classify(text, callback){
         // TODO: Everything
+        callback(null, "ok");
+    }
+
+    calculateProbability(positiveMatches, positiveTotal, negativeMatches, negativeTotal){
+        let unknownWordStrength = 1.0;
+        let unknownWordProbability = 0.5;
+
+        let total = positiveMatches + negativeMatches;
+        let positiveRatio = positiveMatches / parseFloat(positiveTotal);
+        let negativeRatio = negativeMatches / parseFloat(negativeTotal);
+
+        let probability = positiveRatio / (positiveRatio + negativeRatio);
+
+        return ((unknownWordStrength * unknownWordProbability) + (total * probability)) / (unknownWordStrength + total);
+    }
+
+    recordProbability(probability){
+        if (isNaN(probability)) return;
+
+        this.totalProbability = (this.totalProbability === 0) ? probability : this.totalProbability * probability;
+        this.inverseTotalProbability = (this.inverseTotalProbability === 0) ? (1 - probability) : this.inverseTotalProbability * (1 - probability);
+    }
+
+    combineProbabilities(){
+        if (this.totalProbability === 0) return 0.5;
+        return this.totalProbability / (this.totalProbability + this.inverseTotalProbability);
+    }
+
+    evaluateEmotion(probability){
+        if (probability <= (0.5 - this.tolerance)) return "negative";
+        if (probability >= (0.5 + this.tolerance)) return "positive";
+        return "neutral";
     }
 }
 
