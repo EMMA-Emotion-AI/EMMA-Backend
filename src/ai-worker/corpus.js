73bc58f60a916d16814baf861f5fd7b6768aa233
NullDev (Chris) <shadow@nulldev.org>
2019-12-20 12:03:53 +0100

    Base corpus including stopwords

---
+++ src/ai-worker/corpus.js
@@ -0,0 +1,73 @@
+"use strict";
+
+// ====================== //
+// = Copyright (c) EMMA = //
+// ====================== //
+
+// Core Modules
+let path = require("path");
+let fs = require("fs");
+
+// Dependencies
+let rethink = require("rethinkdb");
+
+// Utils
+let config = require("../utils/configHandler").getConfig();
+let log = require("../utils/logger");
+
+let _ = null;
+
+class Corpus {
+    constructor(){
+        this.tokens = {};
+        this.totalTokens = 0;
+    }
+
+    add(document){
+        let self = this;
+        document.eachWord((word) => {
+            self.tokens[word] = (self.tokens[word] || 0) + 1;
+        });
+    }
+
+    loadFromDatabase(){
+        // TODO
+    }
+
+    tokenCount(word){
+        return this.tokens[word] || 0;
+    }
+
+    get stopwords(){
+        return (
+            "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at," +     /* a */
+            "be,because,been,but,by," +                                                      /* b */
+            "can,cannot,could," +                                                            /* c */
+            "dear,did,do,does," +                                                            /* d */
+            "either,else,ever,every," +                                                      /* e */
+            "for,from," +                                                                    /* f */
+            "get,got," +                                                                     /* g */
+            "had,has,have,he,her,hers,him,his,how,however," +                                /* h */
+            "i,if,in,into,is,it,its," +                                                      /* i */
+            "just," +                                                                        /* j */
+            "keep," +                                                                        /* k */
+            "least,let,like,likely," +                                                       /* l */
+            "may,me,might,most,must,my," +                                                   /* m */
+            "neither,no,nor,not," +                                                          /* n */
+            "of,off,often,on,only,or,other,our,own," +                                       /* o */
+            "probably," +                                                                    /* p */
+            "quick," +                                                                       /* q */
+            "rather,really," +                                                               /* r */
+            "said,say,says,she,should,since,so,some," +                                      /* s */
+            "than,that,the,their,them,then,there,these,they,this,tis,to,too,totally," +      /* t */
+            "us," +                                                                          /* u */
+            "view," +                                                                        /* v */
+            "wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would," +  /* w */
+            "" +                                                                             /* x */
+            "yet,you,your,yourself," +                                                       /* y */
+            "zero"                                                                           /* z */
+        ).split(",");
+    }
+}
+
+module.exports = Corpus;
